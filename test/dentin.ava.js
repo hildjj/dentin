57720452e26ffea1b16fac72289e9cd8299e4f32
Joe Hildebrand <joe-github@cursive.net>
2019-09-27 13:05:09 -0600

    Start thinking about coverage and trigger Travis for the first time.

--- test/dentin.ava.js
+++ test/dentin.ava.js
@@ -5,9 +5,9 @@ const Dentin = require('../lib/dentin')
 
 test('dentToString', t => {
   const out = Dentin.dentToString(
-    '<foo xmlns:foo=\'urn:foo\' y=\'n&apos;\'><foo:bar foo:x=\'y\'/></foo>')
+    '<foo xmlns:foo="urn:foo" y="n&apos;&amp;>"><foo:bar foo:x="y"/></foo>')
   t.deepEqual(out, `<?xml version='1.0'?>
-<foo y='n&apos;' xmlns:foo='urn:foo'>
+<foo y='n&apos;&amp;&gt;' xmlns:foo='urn:foo'>
   <foo:bar foo:x='y'/>
 </foo>\n`)
 })
